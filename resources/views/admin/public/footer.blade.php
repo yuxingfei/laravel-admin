b7777315499a62ef27d50fc72ad66126c4401981
yuxingfei <yuxingfei@moerlong.com>
2021-01-08 09:51:00 +0800

    界面显示调整

--- resources/views/admin/public/footer.blade.php
+++ resources/views/admin/public/footer.blade.php
@@ -4,7 +4,7 @@
     <div class="pull-right hidden-xs">
         <b>Version</b> {{ isset($admin['version']) ? $admin['version'] : '1.0' }}
     </div>
-    <strong>Copyright &copy; 2019 <a href="{{ isset($admin['link']) ? $admin['link'] : '#' }}">{{ isset($admin['author']) ? $admin['author'] : 'admin' }}</a>.</strong> All rights
+    <strong>Copyright &copy; @php echo date('Y'); @endphp <a href="{{ isset($admin['link']) ? $admin['link'] : '#' }}">{{ isset($admin['author']) ? $admin['author'] : 'admin' }}</a>.</strong> All rights
     reserved.
 </footer>
 @endif
